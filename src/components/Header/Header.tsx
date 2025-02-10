edb7a2840adcc6533ce0ce640fee03d6d18f1812
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-10 12:07:28 +0000

    add updated Header && update tea map

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -8,11 +8,11 @@ export default function Header(){
         <header>
             <nav>
               <ul className="flex justify-center gap-4 p-7 bg-black text-white font-bold">
-                <li>About</li>
                 <li><Link href="/#Tea_Map">Location</Link></li>
-                <li>Teas</li>
-                <li>Ethical Sourcing</li>
-                <li>ESG Targets</li>
+                <li>About - Placeholder</li>
+                <li>Teas - Placeholder</li>
+                <li>Ethical Sourcing - Placeholder</li>
+                <li>ESG Targets - Placeholder</li>
               </ul>
             </nav>
         </header>
