0865e9cfe0a61e4f612e1cea1b2a4ebfad69ada4
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-12 16:23:21 +0000

    add screen shots && add title

--- README.md
+++ README.md
@@ -11,3 +11,10 @@ npm run dev
 Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
 
 
+## Screenshots
+
+!["Screenshot 1"](Screenshot%20from%202025-02-12%2016-06-02.png)
+
+!["Screenshot 2"](Screenshot%20from%202025-02-12%2016-06-54.png)
+
+!["Screenshot 3"](./Screenshot%20from%202025-02-12%2016-07-27.png)
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,10 +4,16 @@ import Banner from "../components/Banner/Banner"
 import Tea_Map from "../components/Tea_Map/Tea_Map"
 import Footer from "../components/Footer/Footer"
 
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
       
+      <Head>
+        <title>Tea Latte & Co</title>
+      </Head>
+
       <Header/>
       
       <Banner/>
