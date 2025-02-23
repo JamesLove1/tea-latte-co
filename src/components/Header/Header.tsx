9fffe068248c2108100d4703819522877fa030be
James Love Ubuntu Laptop <jimmeylove@gmail.com>
2025-02-23 19:25:33 +0000

    update header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,28 +1,59 @@
+"use client";
+
 import styles from "./Header.module.css"
 import Link from 'next/link'
+import { useEffect, useState } from "react"
 
 
 export default function Header(){
 
+    const [isMenuOpen, setIsMenuOpen] = useState(false)
+
+
     return (
 
         <header style={{position:"sticky", top:"0"}}>
-            <nav className="flex justify-around gap-4 p-7 bg-black text-white font-bold">
+
+            <nav className={styles.navBar}>    
+              <div className={styles.menueAndLogo}>
+                <Link href="/">
+
+                  <div className={styles.logoText}>
+
+                    <img src="./coffee-cup.png" alt="coffee-cup.png" />
+
+                    <h1>Tea Latte & Co</h1>
+
+                  </div>
+
+                </Link>
+
+                <img 
+                  className={styles.menueIcon} 
+                  src="./burger-bar.png"
+                  onClick={() => setIsMenuOpen(!isMenuOpen)}
+                ></img> 
+              </div>
+
               
-              <Link href="/">
-                <div className={styles.logoText}>
+              <ul 
+                className={`${styles.navBarLinks} ${isMenuOpen ? styles.showMenu : ''}`}
+                >
+
+                <li className="p-2">
+                    <Link href="/">Home</Link>
+                </li>
                 
-                  <img src="./coffee-cup.png" alt="coffee-cup.png" />
-                  <h1>Tea Latte & Co</h1>
-                </div>
-              </Link>
+                <li className="p-2">
+                    <Link href="/#Tea_Map">Sourcing Tea</Link>
+                </li>
 
-              <ul className={styles.navBar} >
-                <li className="p-2"><Link href="/">Home</Link></li>
-                <li className="p-2"><Link href="/#Tea_Map">Sourcing Tea</Link></li>
-                <li className="p-2"><Link href="/Tea-Range">Tea Range</Link></li>
+                <li className="p-2">
+                    <Link href="/Tea-Range">Tea Range</Link>
+                </li>
+              
               </ul>
-
+              
             </nav>
         </header>
 
